52dcf98b6269a7c04c72d82ff9e3c67290fad159
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-09-09 01:25:38 -0700

    some libraries installed

--- Server.js
+++ Server.js
@@ -1,36 +1,27 @@
 var express = require("express");
-// var cors = require("cors");
-// var bodyParser = require("body-parser");
+var cors = require("cors");
+var bodyParser = require("body-parser");
 // var dbConnection = require("./config/Db");
 // Routes List
-// var ordersRoute = require("./components/orders/OrdersRoutes");
-// var AuthRoutes = require("./components/auth/AuthRoutes");
+
 
 var app = express();
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
-// app.use(bodyParser.json());
-// app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cors());
 
 // dbConnection();
 
-// app.get("/", function (req, res) {
-//   req.clientData;
-//   // logics main server
-//   res.send("Server is working");
-// });
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", function (req, res) {
+  req.clientData;
+  // logics main server
+  res.send("Server is working ");
 });
 
-// app.use("/orders", ordersRoute);
-// app.use("/auth", AuthRoutes);
-// ip:port/auth/login - post
-// ip:port/orders/createOrder - post
-// ip:port/orders - get
 
 // server port listener
 app.listen("5000", (err) => {
