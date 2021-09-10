d27c00000ad71a448ac76b206ac33eb8cb882a27
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-09-09 23:07:09 -0700

    Email exist check complete

--- components/signup/SignupController.js
+++ components/signup/SignupController.js
@@ -4,37 +4,44 @@ const signupModel = require("./SignupModel")
 // In controller we define our API logics.
 // Here is a controller (signupUser)
 
-module.exports.signupUser = (req, res)=>{
-  
-    // Here we will pass our data to ensure the schema which we defined.
-    const userCreate = new signupModel({
-        email: req.body.email,
-        password: req.body.password,
-    })
+module.exports.signupUser = async(req, res)=>{
+  //  Here we will check is given email is exist in our db or not?
+
+  let userCheck = await signupModel.findOne({email: req.body.email})
+  if(userCheck){
+    res.status(200).send({result: userCheck, message: "Email Already Registered!"})
+  }else{
+    res.send({message: "You can Signup"})
+  }
+    // // Here we will pass our data to ensure the schema which we defined.
+    // const userCreate = new signupModel({
+    //     email: req.body.email,
+    //     password: req.body.password,
+    // })
 
 
-    // Here we will save our data in dataBase.
-    try {
-        userCreate.save((err, success) => {
-          console.log("success", success);
-          console.log("err", err);
-          if (err) {
-            res.send("err happen");
-          }
-          // res.send("User Registered successfully");
-          res.status(200).send({
-            result: success,
-            message: "User Registered successfully!",
-          });
-        });
-    } catch (error) {
-        // console.log("Error in saving data", error);
-        res
-          .status(400)
-          .send({
-            result: error.message,
-            message: "Oops! User not Registered successfully",
-          });
-    }
+    // // Here we will save our data in dataBase.
+    // try {
+    //     userCreate.save((err, success) => {
+    //       console.log("success", success);
+    //       console.log("err", err);
+    //       if (err) {
+    //         res.send("err happen");
+    //       }
+    //       // res.send("User Registered successfully");
+    //       res.status(200).send({
+    //         result: success,
+    //         message: "User Registered successfully!",
+    //       });
+    //     });
+    // } catch (error) {
+    //     // console.log("Error in saving data", error);
+    //     res
+    //       .status(400)
+    //       .send({
+    //         result: error.message,
+    //         message: "Oops! User not Registered successfully",
+    //       });
+    // }
     
 }
