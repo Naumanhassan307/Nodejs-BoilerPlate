ff745f53be8c919fb32966717ea50195811759ec
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-09-15 07:26:35 -0700

    fix some bugs & complete

--- config/Db.js
+++ config/Db.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose")
 function dbConnection() {
   mongoose.connect(
     // Here you will paste your MongoDb String with your user name and password
-    `mongodb+srv://NaumanHassan:nauman30hassan@cluster0.pvabz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    `mongodb+srv://NaumanHassan:<Password>@cluster0.pvabz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     { useNewUrlParser: true,},
     function (err) {
       if (err) {
